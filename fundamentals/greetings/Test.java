edbe45683cef8a83fc64d83928c4531a6f961117
PatrickTDewey <ptddev@protonmail.com>
2021-09-24 19:25:17 -0700

    java hello world added.

---
+++ fundamentals/greetings/Test.java
@@ -0,0 +1,5 @@
+public class Test {
+    public static void main(String[] args){
+        System.out.println("Hello world, My name is Patrick.\r\nI am 28 years old.\r\nMy Hometown is Baltimore MD.");
+    }
+}
